2717ec07d791bb6d8405f41fffb387720f57afec
RenatoValenzuela262 <167827626+RenatoValenzuela262@users.noreply.github.com>
2025-05-26 16:44:17 -0400

    Delete springboot-backend directory

--- springboot-backend/src/main/java/com/proyecto/springboot/backend/springboot_backend/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.proyecto.springboot.backend.springboot_backend.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.proyecto.springboot.backend.springboot_backend.entities.Usuario;
-
-public interface UsuarioRepository extends CrudRepository<Usuario, Long>{
-
-}
